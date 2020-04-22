dfce172df2fa8694055ddacd643a34a46d8c5f36
danieleancora <38218284+danieleancora@users.noreply.github.com>
2020-04-22 20:32:05 +0200

    Create generatedata.py

---
+++ generatedata.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 15:04:08 2020
+
+@author: Daniele Ancora
+"""
+
+# LIBRARIES CALL
+import time
+import cupy as cp
+import numpy as np
+import pyphret.functions as pf
+
+
+def randomGaussianBlobs_2D(dimension=512, fraction=0.999):
+    size = int(dimension/2)
+    size_pad = int(dimension/4)
+
+    test_image = np.random.rand(size,size)
+    test_image = (test_image > 0.999).astype(np.float32) * pf.circular_mask2D(size, size)
+    test_image = np.pad(test_image, (size_pad,size_pad), 'constant')
+    test_image = pf.my_gaussblur(test_image,4)
+
+    return test_image
+
+
+def randomGaussianBlobs_3D(dimension=512, fraction=0.999):
+    size = int(dimension/2)
+    size_pad = int(dimension/4)
+
+    test_image = np.random.rand(size,size,size)
+    test_image = (test_image > 0.999).astype(np.float32) * pf.spherical_mask3D(size, size, size)
+    test_image = np.pad(test_image, (size_pad,size_pad), 'constant')
+    test_image = pf.my_gaussblur(test_image,4)
+
+    return test_image
