64b53a100d87fe706e2c98b2eda22fbe4419eee6
danieleancora <38218284+danieleancora@users.noreply.github.com>
2020-10-02 18:30:21 +0200

    boh

--- generatedata.py
+++ generatedata.py
@@ -92,5 +92,70 @@ def randomAmorphousVases_3D(intdimension=512, extdimension=1024, volumeradius=2*
     sample_vase = pf.axisflip(hyp**2) * pf.my_gaussblur(sample_vase**2, 0.5)   
     sample_vase = np.abs(sample_vase) 
     sample_vase /= sample_vase.max()
+    
+    
 
     return sample_vase
+
+
+
+def randomAmorphousVases_3Dspeckled(intdimension=512, extdimension=1024, volumeradius=2*(64+16), maskradius1=13, maskradius2=11):
+    # definition of 2 concentric masks, used for amorphous generation
+    mask1 = pf.spherical_mask3D(extdimension, extdimension, extdimension, center=None, radius=maskradius1)
+    mask2 = pf.spherical_mask3D(extdimension, extdimension, extdimension, center=None, radius=maskradius2)
+    mask = (np.float32(mask1)-np.float32(mask2))
+
+    # this is the mask that select the size of the phantom
+    mask3 = pf.spherical_mask3D(intdimension,intdimension,intdimension, center=None, radius=volumeradius)
+ 
+    # random phase and amplitude to associate to the mask
+    phase = 2*np.pi*np.random.rand(extdimension,extdimension,extdimension)
+    ampli = 1 - 0.01*np.random.rand(extdimension,extdimension,extdimension)
+    
+    # generation of the volumetric amorphous pattern, this is symmetric
+    randomrods = np.abs(np.fft.fftn( ampli * np.exp(phase * mask)) )
+        
+    # select only the portion up to intdimension within mask3
+    hyp = (randomrods[:intdimension,:intdimension,:intdimension]*mask3)
+    hyp /= hyp.max()
+    hyp_dots = hyp.copy()
+    hyp = 1 - hyp
+    hyp[hyp==1] = 0
+    
+    # creating the outer veins
+    threshold_veins = 0.95
+    sample = np.float32(hyp>threshold_veins)
+    sample = sample*hyp
+    sample = np.float32(sample) - threshold_veins
+    sample[sample<0] = 0
+    sample /= sample.max()
+    
+    # creating the inner veins
+    threshold_veins = 0.93
+    sample_veins = np.float32(hyp>threshold_veins)
+    sample_veins = sample_veins*hyp
+    sample_veins = np.float32(sample_veins) - threshold_veins
+    sample_veins[sample_veins<0] = 0
+    sample_veins /= sample_veins.max()
+    
+    # creating the vases by subtracting the veins
+    sample_vase = np.abs(sample_veins - sample)
+    
+         
+    # smooth and sharpen before returning
+    sample_vase = pf.axisflip(hyp**2) * pf.my_gaussblur(sample_vase**2, 0.5)   
+    sample_vase = np.abs(sample_vase) 
+    
+    randomrods = np.abs(np.fft.fftn( ampli * np.exp(phase * mask2)) )
+    randomrods = randomrods[:intdimension,:intdimension,:intdimension]
+
+    sample_vase = sample_vase * randomrods
+    
+    sample_vase /= sample_vase.max()
+    
+    
+
+    return sample_vase
+
+
+
--- stackoperation.py
+++ stackoperation.py
@@ -29,7 +29,7 @@ def rotateStack(origStack, angleInitial=0, angleStep=90, rotateaxes=(0,1)):
             rotatedStack[i,:,:,:] = ndimage.rotate(origStack[i,:,:,:],   -angleStep*i, axes=rotateaxes, reshape=False)    
         else:
             rotatedStack[i,:,:,:] = np.rot90(origStack[i,:,:,:], -i, axes=rotateaxes)
-        print(i)
+        print('Rotating view ' + str(i) + ' - deg = ' + str(-angleStep*i))
 
     return rotatedStack
 
@@ -45,9 +45,9 @@ def alignStack(origStack, reference=0):
     for i in range(origStack.shape[0]):
         if i != reference:
             alignedStack[i,:,:,:] = pf.my_alignND(origStack[reference,:,:,:], origStack[i,:,:,:])
-            print(i)
         else:
             alignedStack[i,:,:,:] = origStack[i,:,:,:]
+        print('Aligning view ' + str(i) + ' agains reference = ' + str(reference))
 
     return alignedStack
    
@@ -55,16 +55,56 @@ def alignStack(origStack, reference=0):
    
 def autocorrelateStack(origStack):
     # pop out a warning
-    print('Calculating the stack autocorrelation...')
+    print('Calculating the stack auto-correlation...')
 
     # create the stack
-    spimXcorr = np.zeros_like(origStack)
+    spimAcorr = np.zeros_like(origStack)
 
     # calculating autocorrelations from spim detections
     for i in range(origStack.shape[0]):
-        spimXcorr[i,:,:,:] = np.abs(pf.my_autocorrelation(origStack[i,:,:,:]))
-        print(i)
+        spimAcorr[i,:,:,:] = np.abs(pf.my_autocorrelation(origStack[i,:,:,:]))
+        print('Auto-correlating view ' + str(i))
+
+    return spimAcorr
+
+
+def crosscorrelateStack(origStack):
+    # pop out a warning
+    print('Calculating whole stack cross-correlation...')
+
+    # create the stack
+    spimXcorr = np.zeros((origStack.shape[0], origStack.shape[0], origStack.shape[1], origStack.shape[2], origStack.shape[3]))
+
+    # calculating autocorrelations from spim detections
+    for i in range(origStack.shape[0]):
+        for j in range(origStack.shape[0]):
+            spimXcorr[i,j,:,:,:] = np.abs(pf.my_correlationCentered(origStack[i,:,:,:],origStack[j,:,:,:]))
+            print('Cross correlating view ' + str(i) + ' with ' + str(j))
         
     return spimXcorr
+    
 
+
+# i'm not sure this thing is working as it should
+def focusStack(origStack, sigma=2, axis=0):
+    
+    laplaceStack = np.zeros_like(origStack)
+    
+    minvalue = origStack.min()
+    origStack = origStack - minvalue
+    
+    # calculating autocorrelations from spim detections
+    for i in range(origStack.shape[0]):
+        print('Calculating Laplacian of view ' + str(i))
+        # laplaceStack[i,:,:,:] = np.abs(ndimage.laplace(origStack[i,:,:,:]))
+        laplaceStack[i,:,:,:] = np.abs(ndimage.gaussian_filter(ndimage.laplace(origStack[i,:,:,:]), sigma))      
+        # laplaceStack[i,:,:,:] = np.abs(ndimage.gaussian_laplace(origStack[i,:,:,:], sigma=2))
+        
+    
+    index_array = np.argmax(laplaceStack, axis=axis)
+
+    # Same as np.max(x, axis=-1, keepdims=True)
+    focussedStack = np.take_along_axis(origStack, np.expand_dims(index_array, axis=axis), axis=axis)
+     
+    return focussedStack+minvalue
     
